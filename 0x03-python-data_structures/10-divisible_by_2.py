43c2c6efea348878301c0293520cae0a8ad150c4
AnneMbulwa <annembulwa602@gmail.com>
2023-10-11 03:42:19 -0700

    python

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newly_list = []
+    for x in my_list:
+        if a % 2 == 0:
+            newly_list.append(True)
+        else:
+            newly_list.append(False)
+    return newly_list
---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    biggest = my_list[0]
+    for a in range(len(my_list)):
+        if a > biggest:
+            biggest = a
+    return biggest

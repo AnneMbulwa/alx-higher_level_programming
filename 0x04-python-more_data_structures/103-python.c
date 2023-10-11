8aaae1c7ebc454abd91f3c5b09d8c2261d07b87a
AnneMbulwa <annembulwa602@gmail.com>
2023-10-11 06:59:46 -0700

    python

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,22 +9,22 @@
  */
 void print_python_list(PyObject *p)
 {
-    if (!PyList_Check(p))
-    {
-        printf("Error: object is not a Python list\n");
-        return;
-    }
+	if (!PyList_Check(p))
+	{
+		printf("Error: object is not a Python list\n");
+		return;
+	}
+	printf("Python list: size %zu\n", PyList_Size(p));
+	printf("Elements:\n");
 
-    printf("Python list: size %zu\n", PyList_Size(p));
-    printf("Elements:\n");
+	for (Py_ssize_t a = 0; a < PyList_Size(p); a++)
+	{
+		PyObject *element = PyList_GetItem(p, a);
 
-    for (Py_ssize_t a = 0; a < PyList_Size(p); a++)
-    {
-        PyObject *element = PyList_GetItem(p, a);
-        printf(" %zd: ", a);
-        PyObject_Print(element, stdout, 0);
-        printf("\n");
-    }
+		printf(" %zd: ", a);
+		PyObject_Print(element, stdout, 0);
+		printf("\n");
+	}
 }
 
 /**
@@ -34,18 +34,16 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-    if (!PyBytes_Check(p)) {
-        printf("Error: Invalid Bytes Object\n");
-        return;
-    }
-
-    printf("Python bytes: size %zu\n", PyBytes_Size(p));
-    printf("First 10 bytes: ");
-
-    for (Py_ssize_t a = 0; a < PyBytes_Size(p) && a < 10; a++)
-    {
-        printf("%02x ", (unsigned char)PyBytes_AsString(p)[a]);
-    }
-
-    printf("\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("Error: Invalid Bytes Object\n");
+		return;
+	}
+	printf("Python bytes: size %zu\n", PyBytes_Size(p));
+	printf("First 10 bytes: ");
+	for (Py_ssize_t a = 0; a < PyBytes_Size(p) && a < 10; a++)
+	{
+		printf("%02x ", (unsigned char)PyBytes_AsString(p)[a]);
+	}
+	printf("\n");
 }

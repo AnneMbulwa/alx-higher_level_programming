a664bda66972a622315e925b6ab3a5b7a9d69681
AnneMbulwa <annembulwa602@gmail.com>
2023-10-23 04:18:13 -0700

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for a in range(x):
         try:
             print(f"{my_list[a]}", end=" ")
-        except IndexError:
             num += 1
+        except IndexError:
             break
     print()
     return (num)

4a529cf18bf18d463f9a1c3914254abbf2dfe0e1
AnneMbulwa <annembulwa602@gmail.com>
2023-10-24 07:06:53 -0700

    classes and objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""creating a class Square"""
 class Square:
     def __init__(self, size=0):
+        """
+        Args: size must be a int
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 

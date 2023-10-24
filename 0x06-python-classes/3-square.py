8cffde9616f88d18bd4f81fe7d866d8e686ffb8e
AnneMbulwa <annembulwa602@gmail.com>
2023-10-24 07:03:49 -0700

    classes and objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-def __init__(self, size=0):
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+""" create a class Square"""
+class Square:
+    def __init__(self, size=0):
+        """
+            size must be an int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        return(self.__size * self.__size)
+        """ returns the current square area"""
+        return (self.__size * self.__size)

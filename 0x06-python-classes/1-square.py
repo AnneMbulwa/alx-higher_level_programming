5cf345d468c5a81501bce9d1f56d68807e9758cb
AnneMbulwa <annembulwa602@gmail.com>
2023-10-25 02:21:41 -0700

    classes and object

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,11 @@
 """
  a class Square that defines a square by: (based on 0-square.py)
 """
+
+
 class Square:
+    """square body"""
+
     def __init__(self, size):
         """
         description: Private instance attribute: size

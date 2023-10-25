de1d2c909fa71de44d9136a28f8bc7a2e5114a76
AnneMbulwa <annembulwa602@gmail.com>
2023-10-25 03:44:55 -0700

    classes and object

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Square class defination"""
+"""create Square class"""
 
 
 class Square:
-    """square body"""
+    """class square body"""
 
     def __init__(self, size=0):
-        """square contructor 
-        Args: size: length of a side of Square
+        """ 
+        Args: 
+            size(int): length of a side of new Square
         """
         self.__size = size
 
     @property
     def size(self):
-        """"The propery of size as the length 
-        of a side of Square
+        """"
         Raises:
             TypeError: if size != int
             ValueErrorr: if size < 0
         """
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -30,23 +30,29 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Get the area instance to comparators"""
+        """Returns area of cuurent square"""
         return self.__size * self.__size
 
     def __le__(self, other):
+        """Defines the <=(less or equal to) comparison"""
         return self.area() <= other.area()
 
     def __lt__(self, other):
+        """Define the <(less) comparison"""
         return self.area() < other.area()
 
     def __ge__(self, other):
+        """Defines the >=(great or equal to) comparison"""
         return self.area() >= other.area()
 
     def __ne__(self, other):
+        """Defines th !=(not equal to) comparison"""
         return self.area() != other.area()
 
     def __gt__(self, other):
+        """Defines the >(great) comparison"""
         return self.area() > other.area()
 
     def __eq__(self, other):
+        """Defines the ==(equal to) comparison"""
         return self.area() == other.area()

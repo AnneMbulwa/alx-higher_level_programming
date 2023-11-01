bff7c3eba62fffad2382491c4e4d65299f2ba2d7
AnneMbulwa <annembulwa602@gmail.com>
2023-11-01 22:38:49 +0300

    objects

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating a class lockedclass"""
+
+
+class LockedClass:
+    """class body
+    class with no object/class attributes except
+    the new instance attribute is called first_name.
+    """
+    __slots__ = ("first_name")

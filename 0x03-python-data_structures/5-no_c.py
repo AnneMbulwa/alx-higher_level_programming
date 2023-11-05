0875c4cf150e585574947ea924a9fdf0b1173759
AnneMbulwa <annembulwa602@gmail.com>
2023-11-05 17:30:41 +0300

    data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_string = ""
-    for char in range(len(my_string)):
-        if (my_string[char] != 'c' and my_string[char] != 'C'):
-            new_string += my_string[char]
+    for x in range(len(my_string)):
+        if (my_string[x] != 'c' and my_string[x] != 'C'):
+            new_string += my_string[x]
     return new_string

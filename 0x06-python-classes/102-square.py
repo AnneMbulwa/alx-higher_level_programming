54867102fc6eec14afe51b31e5d818ccb9dba619
AnneMbulwa <annembulwa602@gmail.com>
2023-11-05 17:12:20 +0300

    classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
     """
         Args:
-        size(int): length of a side of new Square
+            size(int): length of a side of new Square
         """
         self.size = size
 

2ee8052502854e3c833be6be33394cdb461d7d90
AnneMbulwa <annembulwa602@gmail.com>
2023-11-06 12:54:22 +0300

    inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,7 @@
 function that returns the list of available
 attributes and methods of an object.
 """
+
+
 def lookup(obj):
     return (dir(list))
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,8 @@
 """
 Write a class MyList that inherits from list:
 """
+
+
 class MyList(list):
     def print_sorted(self):
         c = sorted(self)

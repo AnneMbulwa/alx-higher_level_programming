744eaf76f82e6a998bf3fb1e3cd0d4c291b62895
AnneMbulwa <annembulwa602@gmail.com>
2023-11-06 12:58:40 +0300

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,9 @@ Write a class MyList that inherits from list:
 
 
 class MyList(list):
+    """Args:
+        self(int): assume type is int
+    """
     def print_sorted(self):
         c = sorted(self)
         print(c)

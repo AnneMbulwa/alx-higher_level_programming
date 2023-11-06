1487aaee971e05f2a1003d0b5c5921b6c3920639
AnneMbulwa <annembulwa602@gmail.com>
2023-11-06 07:54:38 +0300

    python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,15 +3,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    operators = ["+", "-", "*", "/"]
 
-    for a in range(len(operators)):
-        if operators[a] == "+":
-            results = add(a, b)
-        if operators[a] == "-":
-            results = sub(a, b)
-        if operators[a] == "*":
-            results = mul(a, b)
-        if operators[a] == "/":
-            results = div(a, b)
-        print("{0} {1} {2} = {3}".format(a, operators[a], b, results))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))

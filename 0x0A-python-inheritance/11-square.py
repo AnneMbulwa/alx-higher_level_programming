30608ebfd8ca0ce35644cba47c0c3038f801c7d3
AnneMbulwa <annembulwa602@gmail.com>
2023-11-06 14:55:39 +0300

    inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+"""subclass square of class rectangle"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
+    """vlass square body
+    Args:
+        size(int):
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        """returns the area of current square"""
         return (self.__size ** 2)
 
     def __str__(self):
-        return str("[square] {} / {}".format(self.__size, self.__size))
+        """print() should print, and str() should return
+        the square description: [Square] <width>/<height>
+        """
+        return str("[square] {}/{}".format(self.__size, self.__size))

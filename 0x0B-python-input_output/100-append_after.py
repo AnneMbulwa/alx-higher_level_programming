b34c8a70cbef2816c41d5c08bfc3b28ddd2fac04
AnneMbulwa <annembulwa602@gmail.com>
2023-11-12 17:31:13 +0300

    input/output

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+function that inserts a line of text to a file,
+after each line containing a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Args:
+        must contain with statement
+        don’t need to manage file permission / file doesn't exist exceptions
+    """
+    with open(filename, 'r') as myfile:
+        a = myfile.readlines()
+
+    with open(filename, 'w') as myfile:
+        for line in a:
+            myfile.write(line)
+            if search_string in line:
+                myfile.write(new_string + '\n')

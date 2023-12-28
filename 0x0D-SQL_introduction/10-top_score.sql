75f98219e619c754456012d9ed47d2d98893a7b6
AnneMbulwa <annembulwa602@gmail.com>
2023-12-28 21:18:00 +0300

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,4 @@
---lists all records of the table second_table of the
---database hbtn_0c_0 in your MySQL server.
+-- Lists all records of the table second_table of database hbtn_0c_0.
 SELECT 'score', 'name'
 FROM 'second_table'
 ORDER BY 'score' DESC;
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
---displays the number of records with id = 89 in the
---table first_table of the database hbtn_0c_0 in your MySQL server
+-- Displays number of records with id = 89 in table first_table.
 SELECT COUNT(*) FROM 'first_table' WHERE 'id' = 89;

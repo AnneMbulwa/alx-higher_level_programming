f4b2afe1e8c9b226ec063bb3f6a035bd8fa75989
AnneMbulwa <annembulwa602@gmail.com>
2023-12-28 22:30:09 +0300

    sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- Lists all the cities of California found in hbtn_0d_usa database
-USE `hbtn_0d_usa`;
 -- States table contains only one record where name = California
 -- Results must be sorted in ascending order by cities.id
-SELECT `id`, `name` FROM `cities` WHERE `state_id` = (
+SELECT `id`, `name` FROM `cities` WHERE `state_id` IN (
 	SELECT `id` FROM `states` WHERE `name` = "California") ORDER BY `id` ASC;

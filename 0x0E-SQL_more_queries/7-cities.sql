2087a2ebd4798fcdd5cba3963f3bc4faa6f13cd4
AnneMbulwa <annembulwa602@gmail.com>
2023-12-29 14:02:54 +0300

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Id is INT unique, auto generated, can’t be null and is a primary key
 -- State_id INT, can’t be null and must be FOREIGN KEY that references to id
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+CREATE TABLE IF NOT EXISTS `cities` (
 	`id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`state_id` INT NOT NULL FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`),
 	`name` VARCHAR(256) NOT NULL);

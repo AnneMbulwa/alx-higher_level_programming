7fcc12536b2759097e538252c95ade8a46fd5af9
AnneMbulwa <annembulwa602@gmail.com>
2023-12-29 12:53:04 +0300

    sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,6 @@
 -- First column must be called genre
 -- Second column must be called number_of_shows
 -- Don’t display a genre that doesn’t have any shows linked
-USE hbtn_0d_tvshows;
 SELECT genres.genre AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM genres LEFT JOIN tv_show_genres
 ON genres.id = tv_show_genres.genre_id

8f11e64e649fdaf982a3a22f18ccf16382eb00ac
AnneMbulwa <annembulwa602@gmail.com>
2023-12-29 13:59:34 +0300

    sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 -- Each record should display: tv_shows.title
 SELECT DISTINCT title
-FROM tv_shows JOIN tv_show_genrea
+FROM tv_shows JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
 
 JOIN tv_genres

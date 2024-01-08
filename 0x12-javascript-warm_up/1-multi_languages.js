c102445b47200fa77b9d857240a253f0d5360fce
AnneMbulwa <annembulwa602@gmail.com>
2024-01-08 19:57:31 +0300

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-let num1 = 'C is fun';
-let num2 = 'Python is cool';
-let num3 = 'JavaScript is amazing';
-
-console.log(num1);
-console.log(num2);
-console.log(num3);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');

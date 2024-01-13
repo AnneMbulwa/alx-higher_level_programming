dc644090c3c0d7c00444c35361fe269678daf39f
AnneMbulwa <annembulwa602@gmail.com>
2024-01-13 10:30:33 +0300

    pyhthon orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    username, password, database = argv[1], argv[2], argv[3]
+
+    db = MySQL.connect(
+            host="localhost",
+            user="username",
+            passwd="password",
+            port=3306,
+            database="database"
+            )
+    cur = db.cursor()
+    cur.execute ("SELECT * FROM states ORDER BY state.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=ays.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+    conn = db.cursor()
+
+    conn.execute (
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY state.id ASC")
+
+    rows = conn.fetchall()
+    for row in rows:
+        print(row)

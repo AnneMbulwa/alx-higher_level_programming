a1f579c9b9be57b0649513848788eec3eb40fba3
AnneMbulwa <annembulwa602@gmail.com>
2024-01-25 21:26:05 +0300

    curl

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -2,4 +2,4 @@
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to
 # respond with a message containing You got me!
 
-curl -sX PUT -H "Origin: HorbertonSchool" -d "User_Id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HorbertonSchool" -d "User_Id=98" 0.0.0.0:5000/catch_me

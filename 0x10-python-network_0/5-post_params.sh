d27007791fa365651d80e933ec19a8741619dc59
AnneMbulwa <annembulwa602@gmail.com>
2024-01-25 21:20:08 +0300

    debugging

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -2,4 +2,5 @@
 # using curl, send a POST request to the passed URL with the variable email and
 # subject and their values
 
-curl -s -X -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
+"$1"

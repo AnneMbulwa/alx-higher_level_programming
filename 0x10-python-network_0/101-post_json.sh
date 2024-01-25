4434b6b9172966af9f917f39ac2b3cd8d96e3636
AnneMbulwa <annembulwa602@gmail.com>
2024-01-25 21:49:16 +0300

    curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # with using curl to send a JSON POST request to a URL passed as 1st argv
-curl -s -X POST -d "@2" -H "Content-Type: application/json" "$1"
+curl -s -X POST -d "@$2" -H "Content-Type: application/json" "$1"
